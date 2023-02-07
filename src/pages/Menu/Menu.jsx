ba2eff3d54cfc0e7f78e1e13d3757115fe47799a
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-02-07 21:13:39 +0300

    add footer and menu components

---
+++ src/pages/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PizzaListPage from '../PizzaListPage';
+
+function Menu() {
+  return (
+    <>
+      <h1>Menu</h1>
+      <PizzaListPage />
+    </>
+  );
+}
+
+export default Menu

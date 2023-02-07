891f743f988492ca207e0df4f0316ef47284a687
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-02-07 21:14:06 +0300

    modify navbar and home components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Footer from './Footer'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+      <div>HomePage</div>
+      <Footer />
+    </>
   )
 }
 

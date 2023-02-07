5e21f7a7e75a8ce46f98364e80597516348744d6
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-02-07 21:14:32 +0300

    add routes to pages

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import PizzaListPage from "./pages/PizzaListPage";
 import PizzaPage from "./pages/PizzaPage";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import NotFoundPage from "./pages/NotFoundPage";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu/Menu";
 
 function App() {
   return (
@@ -13,9 +15,11 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<Menu />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/pizzas" element={<PizzaListPage />} />
         <Route path="/pizzas/:pizzaId" element={<PizzaPage />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>

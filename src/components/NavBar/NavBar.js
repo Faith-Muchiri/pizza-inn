ec9f10d27ac029f9e903a715d3ee9a7bcc8107ca
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-02-13 15:55:45 +0300

    add logo to the navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,38 +5,45 @@ import {Link} from 'react-router-dom'
 function NavBar() {
   return (
     <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/" className="navbar-item active">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/menu" className="navbar-item">
-            Menu
-          </Link>
-        </li>
-        <li>
-          <Link to="/services" className="navbar-item">
-            Services
-          </Link>
-        </li>
-        <li>
-          <Link to="/blog" className="navbar-item">
-            Blog
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className="navbar-item">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="navbar-item">
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <div>
+        <Link to="/" className="navbar-item logo">
+          🍕<span>Pizza</span><br/>Delicious
+        </Link>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/" className="navbar-item active">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu" className="navbar-item">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" className="navbar-item">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/blog" className="navbar-item">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="navbar-item">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="navbar-item">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
